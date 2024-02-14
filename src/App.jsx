b832bc8bb357bc62ddfcf547e3424fffb6ba34f8
umaar-kr <umaar.kr@gmail.com>
2024-02-15 01:13:15 +0500

    building the layout of the project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PageNav.jsx
@@ -0,0 +1,27 @@
+import styles from "./PageNav.module.css";
+import Logo from "./Logo.jsx";
+import { NavLink } from "react-router-dom";
+
+function PageNav() {
+  return (
+    <nav className={styles.nav}>
+      <Logo />
+
+      <ul>
+        <li>
+          <NavLink to="/pricing">Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to="/product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={styles.ctaLink}>
+            Login
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default PageNav;
