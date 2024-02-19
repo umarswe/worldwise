b2e2d24d16fafd1bc5ea3ed607e964339f793a35
umaar-kr <umaar.kr@gmail.com>
2024-02-19 17:15:12 +0500

    adding more components and nesting routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
@@ -12,6 +13,12 @@ function App() {
         <Route path="/product" element={<Product />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/app" element={<AppLayout />}>
+          <Route index element={<p>LIST OF CITIES</p>} />
+          <Route path="/app/cities" element={<p>List of cities</p>} />
+          <Route path="/app/countries" element={<p>List of countries</p>} />
+          <Route path="/app/form" element={<p>Form</p>} />
+        </Route>
       </Routes>
     </div>
   );
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,7 +1,14 @@
+import Map from "../components/Map";
+import Sidebar from "../components/Sidebar";
 import styles from "./AppLayout.module.css";
 
 function AppLayout() {
-  return <div className={styles.app}>App</div>;
+  return (
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
+    </div>
+  );
 }
 
 export default AppLayout;
